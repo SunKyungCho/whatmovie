aa539262d9c6888d9defec8df36191c0b27ace77
sunkyung <sunkyungch@gmail.com>
2018-10-11 01:12:57 +0900

    project update

--- src/main/java/me/toybox/repository/MovieRankRepository.java
+++
@@ -1,9 +0,0 @@
-package me.toybox.repository;
-
-
-import me.toybox.domain.MovieRank;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRankRepository extends JpaRepository<MovieRank, String> {
-
-}
