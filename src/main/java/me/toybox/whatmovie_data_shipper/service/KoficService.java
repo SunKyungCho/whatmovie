96119b5a8b5d9f6dc912aa72696515cc203150c8
sunkyung <sunkyungch@gmail.com>
2019-07-24 01:38:27 +0900

    fix: kofic API를 통해 영화 데이터를 받아온다

--- src/main/java/me/toybox/whatmovie_data_shipper/service/KoficService.java
+++ src/main/java/me/toybox/whatmovie_data_shipper/service/KoficService.java
@@ -5,12 +5,11 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectReader;
+import lombok.RequiredArgsConstructor;
 import me.toybox.whatmovie_data_shipper.domain.Movie;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
@@ -18,7 +17,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -28,135 +27,124 @@ import java.util.stream.Collectors;
 @Component
 public class KoficService {
 
+    private final static String START_YEAR = "0000";
+    private final static String MAX_YEAR = "9999";
+    private final static String MOVIE_COUNT = "100";
+    private final static String MOVIE_LIST_URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json";
+    private final static String MOVIE_DETAIL_URL = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json";
 
     private ObjectMapper objectMapper;
-    private RestTemplateBuilder restTemplateBuilder;
+    private RestTemplate restTemplate;
 
-    public KoficService(RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper) {
-        this.restTemplateBuilder = restTemplateBuilder;
-        this.objectMapper = objectMapper;
+    public KoficService() {
+        this.objectMapper = new ObjectMapper();
+        this.restTemplate = new RestTemplateBuilder().build();
     }
 
     @Value("${kofic.key}")
     String myKey;
 
+    public List<String> fetchMovieByPage(int page) {
 
-    Logger logger = LoggerFactory.getLogger(KoficService.class);
+        JsonNode response = restTemplate.getForObject(getMovieListUrlByPage(page), JsonNode.class);
+        JsonNode jsonNode = response.get("movieListResult").get("movieList");
 
-    public List<Movie> fetchMovieByPage(String page) throws IOException {
-
-        String url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json";
-        UriComponentsBuilder uri = UriComponentsBuilder.fromUriString(url)
-                .queryParam("key", myKey)
-                .queryParam("openStartDt", "0000") //0000년부터
-                .queryParam("openEndDt", "3000") // 전체연도를 파악하기 위해
-                .queryParam("itemPerPage", "100")
-                .queryParam("curPage", page);
-
-        RestTemplate restTemplate = restTemplateBuilder.build();
-        String response = restTemplate.getForObject(uri.toUriString(), String.class);
-
-        JsonNode rootNode = objectMapper.readTree(response);
-        JsonNode jsonNode = rootNode.get("movieListResult").get("movieList");
-
-        List<Movie> movies = Collections.emptyList();
-        if(jsonNode.isArray())
-        {
+        List<String> movieCodes = new ArrayList<>();
+        if (jsonNode.isArray()) {
             for (JsonNode node : jsonNode) {
-                if (isPorno(node)) continue; // 성인물 filter
-                String movieCd = node.get("movieCd").asText();
-                movies.add(fetchMovieDetail(movieCd));
+                movieCodes.add(node.get("movieCd").asText());
             }
         }
-        return movies;
+        return movieCodes;
     }
 
-    private Movie fetchMovieDetail(String movieCode) throws IOException {
+    public Movie fetchMovieDetail(String movieCode) throws IOException {
 
-        String url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo.json";
-        UriComponentsBuilder uri = UriComponentsBuilder.fromUriString(url)
-                .queryParam("key", myKey)
-                .queryParam("movieCd", movieCode);
-
-        RestTemplate restTemplate = restTemplateBuilder.build();
-        JsonNode movieDetailNode = restTemplate.getForObject(uri.toUriString(), JsonNode.class);
+        JsonNode movieDetailNode = restTemplate.getForObject(getMovieDetailUrl(movieCode), JsonNode.class);
         JsonNode movieInfo = movieDetailNode.get("movieInfoResult").get("movieInfo");
 
         Movie movie = Movie.builder()
                 .movieCode(movieCode)
+                .name(movieInfo.get("movieNm").asText())
+                .nameEn(movieInfo.get("movieNmEn").asText())
+                .actors(getPeopleName(movieInfo.get("actors")))
+                .director(getPeopleName(movieInfo.get("directors")))
+                .genre(getGenres(movieInfo.get("genres")))
+                .nation(getNations(movieInfo.get("nations")))
+                .rating(getRatings(movieInfo.get("audits")))
+                .openDate(movieInfo.get("openDt").asText())
+                .showTime(movieInfo.get("showTm").intValue())
+                .type(movieInfo.get("typeNm").asText())
+                .productionYear(movieInfo.get("prdtYear").asText())
+                .status(movieInfo.get("prdtStatNm").asText())
                 .build();
-        movie.setMovieCode(movieCode);
-        movie.setName(movieInfo.get("movieNm").asText());
-        movie.setNameEn(movieInfo.get("movieNmEn").asText());
-
-        movie.setActor(getPeopleName(movieInfo.get("actors")));
-        movie.setDirector(getPeopleName(movieInfo.get("directors")));
-        movie.setGenre(getGenres(movieInfo.get("genres")));
-        movie.setNation(getNations(movieInfo.get("nations")));
-        movie.setRating(getRatings(movieInfo.get("audits")));
-
-        movie.setOpenDate(movieInfo.get("openDt").asText());
-        movie.setShowTime(movieInfo.get("showTm").intValue());
-        movie.setType(movieInfo.get("typeNm").asText());
-        movie.setStatus(movieInfo.get("prdtStatNm").asText());
-        movie.setProductionYear(movieInfo.get("prdtYear").asText());
-        movie.setCompany(movieInfo.get("movieNmEn").asText());
-
         return movie;
     }
 
-    private String getImageUrl(String movieCode) throws IOException {
+    private String getMovieDetailUrl(String movieCode) {
+        return UriComponentsBuilder.fromUriString(MOVIE_DETAIL_URL)
+                .queryParam("key", myKey)
+                .queryParam("movieCd", movieCode)
+                .toUriString();
+    }
 
+    private String getMovieListUrlByPage(int page) {
+        return UriComponentsBuilder.fromUriString(MOVIE_LIST_URL)
+                .queryParam("key", myKey)
+                .queryParam("openStartDt", START_YEAR)
+                .queryParam("openEndDt", MAX_YEAR)
+                .queryParam("itemPerPage", MOVIE_COUNT)
+                .queryParam("curPage", page)
+                .toUriString();
+    }
+
+    private String getImageUrl(String movieCode) throws IOException {
         Document doc = Jsoup.connect("http://www.kobis.or.kr/kobis/business/mast/mvie/searchMovieDtl.do?code=" + movieCode).get();
         Elements imageDiv = doc.select(".rollList1 a");
         String onclick = imageDiv.get(0).attr("onclick");
-        Pattern pattern = Pattern.compile("/.*jpg");
-        Matcher matcher = pattern.matcher(onclick);
-        String imageUri = matcher.group();
-
-        return imageUri;
+        Matcher matcher = Pattern.compile("/.*jpg").matcher(onclick);
+        return matcher.group();
     }
 
-
     private String getDescription() {
-
         return null;
     }
 
     private String getNations(JsonNode nations) throws IOException {
-        ObjectReader reader = objectMapper.readerFor(new TypeReference<List<JsonNode>>(){});
+        ObjectReader reader = objectMapper.readerFor(new TypeReference<List<JsonNode>>() {
+        });
         List<JsonNode> nationJsonList = reader.readValue(nations);
         return nationJsonList.stream()
                 .limit(5)
                 .map(actor -> actor.get("nationNm").asText())
                 .collect(Collectors.joining(","));
     }
+
     private String getPeopleName(JsonNode actors) throws IOException {
-        ObjectReader reader = objectMapper.readerFor(new TypeReference<List<JsonNode>>(){});
+        ObjectReader reader = objectMapper.readerFor(new TypeReference<List<JsonNode>>() {
+        });
         List<JsonNode> actorJsonList = reader.readValue(actors);
         return actorJsonList.stream()
                 .limit(5)
                 .map(actor -> actor.get("peopleNm").asText())
                 .collect(Collectors.joining(","));
     }
+
     private String getGenres(JsonNode genre) throws IOException {
-        ObjectReader reader = objectMapper.readerFor(new TypeReference<List<JsonNode>>(){});
+        ObjectReader reader = objectMapper.readerFor(new TypeReference<List<JsonNode>>() {
+        });
         List<JsonNode> genreJsonList = reader.readValue(genre);
         return genreJsonList.stream()
                 .map(actor -> actor.get("genreNm").asText())
                 .collect(Collectors.joining(","));
     }
+
     private String getRatings(JsonNode rating) throws IOException {
-        ObjectReader reader = objectMapper.readerFor(new TypeReference<List<JsonNode>>(){});
+        ObjectReader reader = objectMapper.readerFor(new TypeReference<List<JsonNode>>() {
+        });
         List<JsonNode> ratingJsonList = reader.readValue(rating);
         return ratingJsonList.stream()
                 .map(actor -> actor.get("watchGradeNm").asText())
                 .collect(Collectors.joining(","));
     }
-
-    private Boolean isPorno(JsonNode movie) {
-        String genre = movie.get("genreAlt").asText();
-        return genre.equals("성인물(에로)");
-    }
-
 }
--- src/test/java/me/toybox/whatmovie_data_shipper/service/KoficServiceTest.java
+++ src/test/java/me/toybox/whatmovie_data_shipper/service/KoficServiceTest.java
@@ -1,35 +1,37 @@
 package me.toybox.whatmovie_data_shipper.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import net.bytebuddy.asm.Advice;
+import me.toybox.whatmovie_data_shipper.domain.Movie;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KoficServiceTest {
 
-    @Autowired
     private KoficService koficService;
 
+    @Before
+    public void setUp() throws Exception {
+        koficService = new KoficService();
+    }
+
     @Test
-    public void getMovieCodeList() throws IOException {
-        List<String> codeList = koficService.getMovieCodeList();
-        assertThat(codeList.size()).isNotEqualTo(Collections.EMPTY_LIST);
+    public void getMovieCodeList() {
+        List<String> codeList = koficService.fetchMovieByPage(1);
+        assertThat(codeList.size()).isEqualTo(100);
+    }
+
+    @Test
+    public void getMovieDetailTest() throws IOException {
+        Movie movie = koficService.fetchMovieDetail("20124079");
+        assertThat(movie.getName()).isEqualTo("광해, 왕이 된 남자");
     }
 }
